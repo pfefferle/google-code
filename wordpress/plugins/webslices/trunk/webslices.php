66d428f52dec035242c5c94564cfe10beb976f6b
pfefferle <pfefferle@46d9b22f-1b99-97f8-f5b3-7c039774b3dc>
2010-04-20 20:55:21 +0000

    added some small wordpress plugins

---
+++ wordpress/plugins/webslices/trunk/webslices.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ Plugin Name: Webslices
+ Plugin URI: http://notizblog.org/
+ Description: Webslices for themes using <code>get_class();</code>
+ Version: 0.2
+ Author: Matthias Pfefferle
+ Author URI: http://notizblog.org/
+ */
+
+add_filter('post_class', array('Webslices', 'addSlice'));
+add_filter('wp_head', array('Webslices', 'addHeader'));
+
+class Webslices {
+  function addSlice($class) {
+    if (is_page() || is_single()) {
+      $class[] = "hslice";
+
+    }
+    
+    return $class;
+  }
+
+  function addHeader() {
+    if (is_page() || is_single()) {
+      echo '<link rel="default-slice" type="application/x-hatom" href="'.get_permalink().'#post-'.get_the_ID().'"/>';
+    }
+  }
+}
+
+?>
