6506feeaf5d3ff7c444aa180847e0c948168c35a
pfefferle <pfefferle@46d9b22f-1b99-97f8-f5b3-7c039774b3dc>
2010-05-12 15:41:57 +0000

    some php-doc

--- wordpress/plugins/oexchange/trunk/plugin.php
+++ wordpress/plugins/oexchange/trunk/plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: OExchange
-Plugin URI:
-Description:
-Version: 0.1
+Plugin URI: http://wordpress.org/extend/plugins/oexchange/
+Description: Adds OExchange support to WordPress' "Press This" bookmarklet
+Version: 0.13
 Author: Matthias Pfefferle
 Author URI: http://notizblog.org/
 */
@@ -26,12 +26,13 @@ add_action('init', array('OExchangePlugin', 'init'));
 
 /**
  * OExchange class
- * 
+ *
  * @author Matthias Pfefferle
+ * @link http://www.oexchange.org/spec/ OExchange Spec
  */
 class OExchangePlugin {
   /**
-   * add 'oexchange' as a valid query variables.
+   * add 'oexchange' as a valid query var.
    *
    * @param array $vars
    * @return array
@@ -41,35 +42,43 @@ class OExchangePlugin {
 
     return $vars;
   }
-  
+
+  /**
+   * Runs after WordPress has finished loading but
+   * before any headers are sent. Useful for intercepting $_GET or $_POST triggers.
+   */
   function init() {
     $bookmarkletUrl = admin_url('press-this.php');
-    
+
     $thisUrl = 'http'.(isset($_SERVER["HTTPS"]) ? $_SERVER["HTTPS"] == 'off' ? '' : 's' : '').
                     '://'.
                     $_SERVER['HTTP_HOST'].
                     $_SERVER['REQUEST_URI'];
-    
+
     if (stristr($thisUrl, $bookmarkletUrl) === false) {
       return;
     }
-    
+
+    // oexchange to wordpress mapping
     if (isset( $_GET['url'] ))
       $_GET['u'] = $_GET['url'];
-      
+
     if (isset( $_GET['title'] ))
       $_GET['t'] = $_GET['title'];
-    
+
     if (isset( $_GET['description'] ))
       $_GET['s'] = $_GET['description'];
-      
+
     if (isset( $_GET['ctype'] )) {
       if ($_GET['ctype'] == "image") {
         $_GET['i'] = $_GET['imageurl'];
       }
     }
   }
-  
+
+  /**
+   * parse request and show xrd file
+   */
   function parseRequest() {
     global $wp_query, $wp;
 
@@ -77,45 +86,55 @@ class OExchangePlugin {
       if ($wp->query_vars['oexchange'] == 'xrd') {
         $xrd = OExchangePlugin::createXrd();
         header('Content-Type: application/xrd+xml; charset=' . get_option('blog_charset'), true);
-        //header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
         echo $xrd;
         exit;
       }
     }
   }
-  
+
+  /**
+   * generates the xrd file
+   *
+   * @link http://www.oexchange.org/spec/#discovery-targetxrd
+   * @return string
+   */
   function createXrd() {
     $xrd  = "<?xml version='1.0' encoding='UTF-8'?>";
     $xrd .= '<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">';
     $xrd .= '  <Subject>'.'</Subject>';
     $xrd .= '  <Property
         type="http://www.oexchange.org/spec/0.8/prop/vendor">'.get_option('blogname').'</Property>';
-    $xrd .= '  <Property 
+    $xrd .= '  <Property
         type="http://www.oexchange.org/spec/0.8/prop/title">'.get_option('blogdescription').'</Property>';
-    $xrd .= '  <Property 
+    $xrd .= '  <Property
         type="http://www.oexchange.org/spec/0.8/prop/name">"Press This" bookmarklet</Property>';
-    $xrd .= '  <Property 
+    $xrd .= '  <Property
         type="http://www.oexchange.org/spec/0.8/prop/prompt">Press This</Property>';
 
-    $xrd .= '  <Link 
-        rel= "icon" 
+    $xrd .= '  <Link
+        rel= "icon"
         href="'.get_option( 'siteurl' ).'/favicon.ico"
-        type="image/vnd.microsoft.icon" 
+        type="image/vnd.microsoft.icon"
         />';
 
-    $xrd .= '  <Link 
-        rel= "http://www.oexchange.org/spec/0.8/rel/offer" 
+    $xrd .= '  <Link
+        rel= "http://www.oexchange.org/spec/0.8/rel/offer"
         href="'.admin_url('press-this.php').'"
-        type="text/html" 
+        type="text/html"
         />';
     $xrd .= '</XRD>';
-    
+
     return $xrd;
   }
-  
+
+  /**
+   * generates the host-meta/webfinger xrd-link
+   *
+   * @link http://www.oexchange.org/spec/#discovery-hostmeta
+   */
   function hostMetaXrd() {
-    echo '<Link 
-            rel="http://oexchange.org/spec/0.8/rel/resident-target" 
+    echo '<Link
+            rel="http://oexchange.org/spec/0.8/rel/resident-target"
             type="application/xrd+xml"
             href="'.get_option( 'siteurl' ).'/?oexchange=xrd" />'."\n";
   }
