fc57c3d5db4f1ca2db22e1ffc7c1b915ba0bd7e1
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2010-05-17 20:06:06 +0000

    added new version

--- chrome/activitystream/contentscript.js
+++ chrome/activitystream/contentscript.js
@@ -3,8 +3,8 @@ findActivityStreamLinks();
 // See if the document contains a <link> tag within the <head> and
 // whether that points to an RSS feed.
 function findActivityStreamLinks() {
-  // Find all the RSS link elements.
-  var result = document.evaluate('//*[local-name()="link"][contains(@rel, "activitystream")]', document, null, 0, null);
+  // Find all the activity stream link elements.
+  var result = document.evaluate('//*[local-name()="link"][contains(@rel, "activitystream") or contains(@class, "activitystream")]', document, null, 0, null);
 
   var feeds = [];
   var item;
@@ -15,7 +15,7 @@ function findActivityStreamLinks() {
   }
 
   if (count > 0) {
-    // Notify the extension needs to show the RSS page action icon.
+    // Notify the extension needs to show the activity streams action icon.
     chrome.extension.sendRequest({msg: "activityIcon", feeds: feeds});
   }
 }
